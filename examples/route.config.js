c3db53ec6566c5ebe2a9a61f196680280e0f9179
边浩 <binnull@foxmail.com>
2018-01-08 11:17:58 +0800

    layout组件完善

---
+++ examples/route.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: bianhao 
+ * @Date: 2018-01-05 11:19:37 
+ * @Last Modified by: bianhao
+ * @Last Modified time: 2018-01-05 11:21:18
+ */
+import layout from './pages/layout.vue';
+
+export default [
+  {
+    path: '/layout',
+    component: layout
+  }
+]
