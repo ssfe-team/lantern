080d9991bec3e56e01c8a57efd0a49eef72107fa
zhangdi <zhangdichn@gmail.com>
2018-02-24 11:01:40 +0800

    滚动条组件调整

--- packages/scroll/scroll.vue
+++ packages/scroll/scroll.vue
@@ -1,65 +1,79 @@
 <template>
-  <div  @scroll.native=""
+<!--   <div  @scroll.native=""
         class="scroll-wrap"
         ref="wrap"
         :style="{
           'padding-right': style.barWidth + 'px',
           'padding-bottom': style.barHeight + 'px'
         }"
-  >
+  > -->
     <div  class="scroll-container"
-          ref="container"
-          @scroll.stop="setScrollLeftTop"
+          ref="wrap"
+          @scroll.stop=""
+          :style="{
+            'width': style.wrapWidth,
+            'height': style.wrapHeight,
+          }"
     >
-      <div class="scroll-content" ref="inner">
-        <slot></slot>
+      <div class="scroll-content" ref="inner"
+          @scroll.stop="setScrollLeftTop"
+          :style="{
+            'padding-right': style.barWidth + 'px',
+            'padding-bottom': style.barHeight + 'px'
+          }">
+        <slot
+          @scroll.stop=""
+          @scrollReload="scrollReload"
+        ></slot>
+      </div>
+      <div  class="scroll-track scroll-right"
+            v-if="rightBarShow"
+            :style="{
+              'background-color': style.trackColor,
+              'width': style.barWidth + 'px',
+              'bottom': 0 + 'px',
+              'zIndex': style.zIndex,
+              'border-radius': style.barWidth / 2 + 'px'
+            }"
+            :class="{'auto-hide': style.autoHide && !barMousedown}"
+            @mousedown.stop="rightMousedown"
+      >
+        <div  class="scroll-bar"
+              :class="{'right-bar-active': rightActive}"
+              :style="{
+              'background-color': style.barColor,
+              'width': style.barWidth + 'px',
+              'opacity': style.barOpacityInactive,
+              'height': barHPercent * 100 + '%',
+              'top': barTop + 'px'
+              }"
+        ></div>
+      </div>
+      <div  class="scroll-track scroll-bottom"
+            v-if="bottomBarShow"
+            :style="{
+              'background-color': style.trackColor,
+              'height': style.barHeight + 'px',
+              'right': 0 + 'px',
+              'zIndex': style.zIndex,
+              'border-radius': style.barHeight / 2 + 'px'
+            }"
+            :class="{'auto-hide': style.autoHide && !barMousedown}"
+            @mousedown.stop="bottomMousedown"
+      >
+        <div  class="scroll-bar"
+              :class="{'bottom-bar-active': bottomActive}"
+              :style="{
+                'background-color': style.barColor,
+                'width': barWPercent * 100 + '%',
+                'opacity': style.barOpacityInactive,
+                'height': style.barHeight + 'px',
+                'left': barLeft + 'px'
+              }"
+        ></div>
       </div>
     </div>
-    <div  class="scroll-right"
-          v-if="rightBarShow"
-          :style="{
-            'background-color': style.trackColor,
-            'width': style.barWidth + 'px',
-            'bottom': 0 + 'px',
-            'zIndex': style.zIndex,
-            'border-radius': style.barWidth / 2 + 'px'
-          }"
-          @mousedown.stop="rightMousedown"
-    >
-      <div  class="scroll-bar"
-            :class="{'right-bar-active': rightActive}"
-            :style="{
-            'background-color': style.barColor,
-            'width': style.barWidth + 'px',
-            'opacity': style.barOpacityInactive,
-            'height': barHPercent * 100 + '%',
-            'top': barTop + 'px'
-            }"
-      ></div>
-    </div>
-    <div  class="scroll-bottom"
-          v-if="bottomBarShow"
-          :style="{
-            'background-color': style.trackColor,
-            'height': style.barHeight + 'px',
-            'right': 0 + 'px',
-            'zIndex': style.zIndex,
-            'border-radius': style.barHeight / 2 + 'px'
-          }"
-          @mousedown.stop="bottomMousedown"
-    >
-      <div  class="scroll-bar"
-            :class="{'bottom-bar-active': bottomActive}"
-            :style="{
-              'background-color': style.barColor,
-              'width': barWPercent * 100 + '%',
-              'opacity': style.barOpacityInactive,
-              'height': style.barHeight + 'px',
-              'left': barLeft + 'px'
-            }"
-      ></div>
-    </div>
-  </div>
+  <!-- </div> -->
 </template>
 <script>
 export default {
@@ -74,6 +88,9 @@ export default {
         barOpacityInactive: 0.3,
         trackColor: '#eee',
         zIndex: 'auto',
+        wrapWidth: '100%',
+        wrapHeight: '100%',
+        autoHide: 1
       },
       // 是否显示右侧滚动条
       rightBarShow: false,
@@ -113,7 +130,6 @@ export default {
   },
   mounted() {
     Object.assign(this.style, this.barStyle);
-    this.setWrapSize();
     this.getPercent();
   },
   watch: {
@@ -130,21 +146,16 @@ export default {
     }
   },
   methods: {
-    setWrapSize() {
-      this.wrapSize = {
-        width: this.$refs.wrap.clientWidth + 0 + 'px',
-        height: this.$refs.wrap.clientHeight + 0 + 'px'
-      }
+    scrollReload() {
+      this.getPercent();
     },
     getPercent() {
       const wrapNode = this.$refs.wrap;
-      // const slotnode = this.$slots.default[0]['elm'];
       const slotnode = this.$refs.inner;
       this.wrapHeight = wrapNode.clientHeight - this.style.barHeight;
       this.wrapWidth = wrapNode.clientWidth - this.style.barWidth;
       this.innerHeight = slotnode.scrollHeight;
       this.innerWidth = slotnode.scrollWidth;
-      console.log(slotnode)
       if(this.wrapHeight < this.innerHeight) {
         this.barHPercent = this.wrapHeight / this.innerHeight;
         this.rightBarShow = true;
@@ -190,7 +201,7 @@ export default {
         newTop = newTop > (1 - this.barHPercent) * this.wrapHeight ? (1 - this.barHPercent) * this.wrapHeight : newTop;
         percent = newTop / ((1 - this.barHPercent) * this.wrapHeight);
         scrollTop = (this.innerHeight - this.wrapHeight) * percent;
-        this.$refs.container.scrollTop = scrollTop;
+        this.$refs.inner.scrollTop = scrollTop;
         this.scrollTop = scrollTop;
       }
 
@@ -227,7 +238,7 @@ export default {
         newLeft = newLeft > (1 - this.barWPercent) * this.wrapWidth ? (1 - this.barWPercent) * this.wrapWidth : newLeft;
         percent = newLeft / ((1 - this.barWPercent) * this.wrapWidth);
         scrollLeft = (this.innerWidth - this.wrapWidth) * percent;
-        this.$refs.container.scrollLeft = scrollLeft;
+        this.$refs.inner.scrollLeft = scrollLeft;
         this.scrollLeft = scrollLeft;
       }
 
@@ -252,77 +263,98 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.scroll-wrap {
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  position: relative;
+// .scroll-wrap {
+//   width: 100%;
+//   height: 100%;
+//   overflow: hidden;
+//   position: relative;
   .scroll-container {
     width: 100%;
     height: 100%;
-    overflow: auto;
+    overflow: hidden;
+    position: relative;
     &::-webkit-scrollbar {
       display: none;
     }
     .scroll-content {
       // border-right: 20px solid transparent;
       // border-bottom: 20px solid transparent;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      box-sizing: border-box;
       position: relative;
       padding-top: 1px;
       padding-bottom: 1px;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+      &::-moz-scrollbar {
+        display: none;
+      }
+      &::-o-scrollbar {
+        display: none;
+      }
     }
-  }
-  .scroll-right, .scroll-bottom {
-    background-color: #eee;
-    opacity: 1;
-    position: absolute;
-    transition: opacity .34s ease-out;
-    -webkit-transition: opacity .34s ease-out;
-  }
-  .scroll-bar {
-    opacity: 0.3;
-    position: relative;
-    display: block;
-    cursor: pointer;
-    border-radius: inherit;
-    background-color: #959595;
-    transition: opacity .34s ease-out;
-    -webkit-transition: opacity .34s ease-out;
-    -moz-transition: opacity .34s ease-out;
-    -o-transition: opacity .34s ease-out;
-
-  }
-  .scroll-right {
-    width: 6px;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    .scroll-bar {
+    .scroll-track {
+      background-color: #eee;
+      opacity: 1;
+      position: absolute;
+      transition: opacity .34s ease-out;
+      -webkit-transition: opacity .34s ease-out;
+      &[class~="auto-hide"] {
+        opacity: 0;
+      }
+      .scroll-bar {
+        opacity: 0.3;
+        position: relative;
+        display: block;
+        cursor: pointer;
+        border-radius: inherit;
+        background-color: #959595;
+        transition: opacity .34s ease-out;
+        -webkit-transition: opacity .34s ease-out;
+        -moz-transition: opacity .34s ease-out;
+        -o-transition: opacity .34s ease-out;
+      }
+    }
+    &:hover {
+      .scroll-track {
+        opacity: 1;
+      }
+    }
+    .scroll-right {
       width: 6px;
-      transform: translateY(0%);
-      &:hover {
-        opacity: 0.8 !important;
-      }
-      &[class~="right-bar-active"] {
-        opacity: 0.8 !important;
+      top: 0;
+      bottom: 0;
+      right: 0;
+      .scroll-bar {
+        width: 6px;
+        transform: translateY(0%);
+        &:hover {
+          opacity: 0.8 !important;
+        }
+        &[class~="right-bar-active"] {
+          opacity: 0.8 !important;
+        }
       }
     }
-  }
-  .scroll-bottom {
-    height: 6px;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    .scroll-bar {
+    .scroll-bottom {
       height: 6px;
-      transform: translateX(0%);
-      &:hover {
-        opacity: 0.8 !important;
-      }
-      &[class~="bottom-bar-active"] {
-        opacity: 0.8 !important;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      .scroll-bar {
+        height: 6px;
+        transform: translateX(0%);
+        &:hover {
+          opacity: 0.8 !important;
+        }
+        &[class~="bottom-bar-active"] {
+          opacity: 0.8 !important;
+        }
       }
     }
   }
-}
+// }
 </style>
