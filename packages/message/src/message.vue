860c45492c73aad4a910c8566fd77386236f1e3b
Chuanfeng <wfhtzcf@gmail.com>
2018-02-25 16:30:32 +0800

    格式化代码

--- packages/message/src/message.vue
+++ packages/message/src/message.vue
@@ -24,92 +24,93 @@
 </template>
 
 <script type="text/babel">
-  const typeMap = {
-    success: 'confirm',
-    info: 'details',
-    warning: 'details',
-    error: 'cancel'
-  }
+const typeMap = {
+  success: 'confirm',
+  info: 'details',
+  warning: 'details',
+  error: 'cancel'
+}
 
-  export default {
-    name: 'Message',
-    data () {
-      return {
-        visible: false,
-        message: '',
-        duration: 3000,
-        type: 'info',
-        iconClass: '',
-        customClass: '',
-        onClose: null,
-        showClose: false,
-        closed: false,
-        timer: null,
-        dangerouslyUseHTMLString: false,
-        center: false
+export default {
+  name: 'Message',
+  data() {
+    return {
+      visible: false,
+      message: '',
+      duration: 3000,
+      type: 'info',
+      iconClass: '',
+      customClass: '',
+      onClose: null,
+      showClose: false,
+      closed: false,
+      timer: null,
+      dangerouslyUseHTMLString: false,
+      center: false
+    }
+  },
+
+  props: {},
+
+  computed: {
+    typeClass() {
+      return this.type && !this.iconClass
+        ? `lt-message__icon icon-${typeMap[this.type]}`
+        : ''
+    }
+  },
+
+  watch: {
+    closed(newVal) {
+      if (newVal) {
+        this.visible = false
+        this.$el.addEventListener('transitionend', this.destroyElement)
+      }
+    }
+  },
+
+  methods: {
+    destroyElement() {
+      this.$el.removeEventListener('transitionend', this.destroyElement)
+      this.$destroy(true)
+      this.$el.parentNode.removeChild(this.$el)
+    },
+
+    close() {
+      this.closed = true
+      if (typeof this.onClose === 'function') {
+        this.onClose(this)
       }
     },
 
-    props: {},
-
-    computed: {
-      typeClass () {
-        return this.type && !this.iconClass
-          ? `lt-message__icon icon-${ typeMap[this.type] }`
-          : ''
-      }
+    clearTimer() {
+      clearTimeout(this.timer)
     },
 
-    watch: {
-      closed (newVal) {
-        if (newVal) {
-          this.visible = false
-          this.$el.addEventListener('transitionend', this.destroyElement)
-        }
-      }
-    },
-
-    methods: {
-      destroyElement () {
-        this.$el.removeEventListener('transitionend', this.destroyElement)
-        this.$destroy(true)
-        this.$el.parentNode.removeChild(this.$el)
-      },
-
-      close () {
-        this.closed = true
-        if (typeof this.onClose === 'function') {
-          this.onClose(this)
-        }
-      },
-
-      clearTimer () {
-        clearTimeout(this.timer)
-      },
-
-      startTimer () {
-        if (this.duration > 0) {
-          this.timer = setTimeout(() => {
-            if (!this.closed) {
-              this.close()
-            }
-          }, this.duration)
-        }
-      },
-      keydown (e) {
-        if (e.keyCode === 27) { // esc关闭消息
+    startTimer() {
+      if (this.duration > 0) {
+        this.timer = setTimeout(() => {
           if (!this.closed) {
             this.close()
           }
-        }
+        }, this.duration)
       }
     },
-    mounted () {
-      this.startTimer()
-      document.addEventListener('keydown', this.keydown)
-    },
-    beforeDestroy () {
-      document.removeEventListener('keydown', this.keydown)
+    keydown(e) {
+      if (e.keyCode === 27) {
+        // esc关闭消息
+        if (!this.closed) {
+          this.close()
+        }
+      }
     }
+  },
+  mounted() {
+    this.startTimer()
+    document.addEventListener('keydown', this.keydown)
+  },
+  beforeDestroy() {
+    document.removeEventListener('keydown', this.keydown)
   }
+}
 </script>
