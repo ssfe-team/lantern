81385674fa34fa8b4315720a2c42c1af61ef060f
边浩 <binnull@foxmail.com>
2018-02-26 20:37:50 +0800

    增加class标准

--- examples/route.config.js
+++ examples/route.config.js
@@ -2,7 +2,7 @@
  * @Author: bianhao 
  * @Date: 2018-01-05 11:19:37 
  * @Last Modified by: bianhao
- * @Last Modified time: 2018-02-05 15:27:43
+ * @Last Modified time: 2018-02-26 17:42:29
  */
 import color from './document/color.md';
 import layout from './document/layout.md';
@@ -12,6 +12,7 @@ import message from './document/message.md';
 import pagination from './document/pagination.md';
 import dropdown from './document/dropdown.md';
 import iconfont from './document/iconfont.md';
+import classstandard from './document/classstandard.md';
 
 export default [
   {
@@ -45,5 +46,9 @@ export default [
   {
     path: '/iconfont',
     component: iconfont
+  },
+  {
+    path: '/classstandard',
+    component: classstandard
   }
 ]
