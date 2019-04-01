a8206eaf0ed051b163910e4cfbb89058b02f333a
yuzhe <30771455+Cynthia1911@users.noreply.github.com>
2019-04-01 18:58:03 +0800

    modal添加关闭按钮

--- packages/modal/src/modal.vue
+++ packages/modal/src/modal.vue
@@ -1,8 +1,9 @@
-<template> 
+<template>
   <div class="lt-modal" v-if="show">
     <div class="lt-modal__mask" v-if="show" @click.self="close"></div>
     <div class="lt-modal__content" v-if="show">
       <slot></slot>
+      <lt-icon class="lt-modal__close" type="close" size="22" @click="close"></lt-icon>
     </div>
   </div>
 </template>
