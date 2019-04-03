99452d30221457cbfcbaeec7a8c32f31be6b919a
Chuanfeng <wfhtzcf@gmail.com>
2019-04-03 11:37:26 +0800

    修复刷新页面后 menu-item active 失效

--- packages/menu/src/menuItem.vue
+++ packages/menu/src/menuItem.vue
@@ -2,51 +2,52 @@
   <li v-if="isOut && href" class="lt-menu-item">
     <a :href="href">
       <div>
-        <slot></slot>
+        <slot />
       </div>
     </a>
   </li>
   <router-link class="lt-menu-item" :to="href" tag="li" v-else-if="href">
     <a :href="href">
       <div @click="itemClick">
-        <slot></slot>
+        <slot />
       </div>
     </a>
   </router-link>
   <li v-else class="lt-menu-item">
     <a>
       <div @click="itemClick">
-        <slot></slot>
+        <slot />
       </div>
     </a>
   </li>
 </template>
 <script>
-import Emitter from '../../../src/mixins/emitter.js';
+import Emitter from '../../../src/mixins/emitter.js'
 export default {
   name: 'MenuItem',
   mixins: [ Emitter ],
   props: {
     href: {
-      type: String
+      type: String,
+      default: ''
     }
   },
   computed: {
-    isOut() {
-      if(/\/\//.test(this.href)) {
+    isOut () {
+      if (/\/\//.test(this.href)) {
         return true
       }
       return false
     }
   },
   methods: {
-    itemClick(eve) {
+    itemClick (eve) {
       this.dispatch('Menu', 'updateActiveName', this.$parent._uid)
       this.$emit('itemClick', eve)
     }
   },
-  mounted() {
-    if(this.href == this.$route.path) {
+  mounted () {
+    if (this.href === location.pathname) {
       this.dispatch('Menu', 'updateActiveName', this.$parent._uid)
     }
   }
